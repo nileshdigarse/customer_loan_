76990ae451c42cee84e9284d81fc709c24fd80fa
developer <himanshu60.k@gmail.com>
2023-02-22 19:48:21 +0530

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_113224) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_22_140139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,7 +79,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_113224) do
   end
 
   create_table "no_emis", force: :cascade do |t|
-    t.date "month"
+    t.integer "month"
     t.float "principal"
     t.float "interest_amount"
     t.float "balance"

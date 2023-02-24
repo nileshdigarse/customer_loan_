c0f6cb4ccaa4616cf8f0931587c876a9132615d4
developer <himanshu60.k@gmail.com>
2023-02-24 18:30:24 +0530

    changes

--- app/controllers/emis_controller.rb
+++ app/controllers/emis_controller.rb
@@ -4,8 +4,25 @@ class EmisController < ApplicationController
     @emis = Emi.where(loan_id: @loan.id).order(month: :asc)
   end
 
-  def show
-    debugger
+  def edit
     @emi = Emi.find(params[:id])
+  end
+
+  def update
+    @emi = Emi.find(params[:id])
+    @loan = Loan.find(@emi.loan_id)
+    @customer = Customer.find(@loan.customer_id)
+    byebug
+    if @emi.update(emi_params)
+      redirect_to customer_loan_emis_path(loan_id:@loan.id, customer_id:@customer.id) , notice: 'Emi Paid successfully.'
+    else
+      render json: { errors: @emi.errors }, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def emi_params
+    params.require(:emi).permit( :paid_at, :status)
   end
 end

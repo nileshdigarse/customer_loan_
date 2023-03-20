4630458b5c3eb948ae55d367b211c4a74bec10c0
developer <himanshu60.k@gmail.com>
2023-03-20 16:37:19 +0530

    created investor_transaction model associated with investor table

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -1,4 +1,5 @@
 class Investor < ApplicationRecord
   has_one :address, as: :addressable, dependent: :destroy
   has_one :document, as: :documentable, dependent: :destroy
+  has_many :investor_transactions, dependent: :destroy
 end

3da055bec7e9771fcfd1e8c1711c5d3403898441
developer <himanshu60.k@gmail.com>
2023-04-02 16:06:17 +0530

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,78 +1,73 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.17.2"
+# config valid only for current version of Capistrano
+lock '3.5.0'
 
-set :application, "customer_loan_database"
-set :repo_url, "https://github.com/himanshu123456789000/customer_loan_database.git"
-set :rvm_ruby_version, "ruby-3.0.0"
-set :default_env, { rvm_bin_path: "~/.rvm/bin" }
-set :deploy_to, '/home/ubuntu/customer_loan_database'
-set :use_sudo, true
-set :branch, 'main'
-set :linked_files, %w{config/database.yml}
-set :rails_env, 'production'
-set :keep_releases, 2
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-# Define the stages that you want to deploy to
-set :stages, %w(production staging)
+set :application, 'customer_loan_database'
+set :repo_url, 'git@github.com:himanshu123456789000/customer_loan_database.git' # Edit this to match your repository
+set :branch, :master
+set :deploy_to, '/home/deploy/customer_loan_database'
+set :pty, true
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :keep_releases, 5
+set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-3.0.2' # Edit this if you are using MRI Ruby
 
-# Set the default stage
-set :default_stage, 'staging'
-
-# Load the appropriate stage based on the current environment
-require 'capistrano/ext/multistage'
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.access.log"
-set :puma_error_log,  "#{release_path}/log/puma.error.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/himanshu_ssh/id_rsa.pub) }
-set :puma_preload_app, true
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_access_log, "#{shared_path}/log/puma_error.log"
+set :puma_error_log, "#{shared_path}/log/puma_access.log"
+set :puma_role, :app
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+set :puma_threads, [0, 8]
+set :puma_workers, 0
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-namespace :puma do
-desc 'Create Directories for Puma Pids and Socket'
-task :make_dirs do
-on roles(:app) do
-execute "mkdir #{shared_path}/tmp/sockets -p"
-execute "mkdir #{shared_path}/tmp/pids -p"
-end
-end
-before :start, :make_dirs
-end
+set :puma_init_active_record, true
+set :puma_preload_app, false
 
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git
+# set :scm, :git
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server 'db.example.com', user: 'deploy', roles: %w{db}
 
 
 
@@ -13,7 +13,7 @@
 # ==================
 
 # Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
+# group is considered to be the first unless any  hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
@@ -42,26 +42,22 @@
 # Global options
 # --------------
 #  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
 #    forward_agent: false,
 #    auth_methods: %w(password)
 #  }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server "example.com",
-#   user: "user_name",
+# server 'example.com',
+#   user: 'user_name',
 #   roles: %w{web app},
 #   ssh_options: {
-#     user: "user_name", # overrides user setting above
+#     user: 'user_name', # overrides user setting above
 #     keys: %w(/home/user_name/.ssh/id_rsa),
 #     forward_agent: false,
 #     auth_methods: %w(publickey password)
-#     # password: "please use keys"
+#     # password: 'please use keys'
 #   }
-server '172.31.87.136', user: 'ubuntu', roles: %w{web app db}
-set :ssh_options, {
-forward_agent: true,
-auth_methods: %w[publickey],
-keys: %w[/home/developer/Downloads/loan.pem]
-}
+
+server '52.32.208.85', user: 'deploy', roles: %w{web app db}

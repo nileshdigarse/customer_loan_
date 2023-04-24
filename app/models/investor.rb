84b0a1b60b2997599d1b1e65858ebb599708ba5e
developerdeepika <deepikadangi9060@gmail.com>
2023-04-24 16:46:31 +0530

    Investor functionality

--- app/models/investor.rb
+++ app/models/investor.rb
@@ -3,6 +3,9 @@ class Investor < ApplicationRecord
   has_one :document, as: :documentable, dependent: :destroy
   has_many :investor_transactions, dependent: :destroy
 
+  accepts_nested_attributes_for :document, allow_destroy: true
+  accepts_nested_attributes_for :address, allow_destroy: true
+
   validates :name, :contact_no, :email, :profit_amount, :total_amount, :status, presence: true
   validates :email, uniqueness: true
   validates :contact_no, length: { is: 10 }

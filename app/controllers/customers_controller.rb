e125544af5c32fc84b1120d99ce6de8d4dd9ec5a
Ankita <ankita.chourasiya1105@gmail.com>
2023-05-02 19:26:10 +0530

    Integrate edit functionality on customer.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -36,6 +36,16 @@ class CustomersController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def edit
+    @customer = Customer.find(params["id"])
+  end
+
+  def update
+    @customer = Customer.find(params["id"])
+    @customer.update(customer_params)
+    redirect_to customers_path, flash: { success: 'Update Customer Profile Successfully' }
+  end
   
   private    
   # def customer_params
